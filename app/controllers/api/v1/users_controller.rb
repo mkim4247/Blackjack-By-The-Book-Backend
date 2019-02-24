a451d28655c598e3b6d6ee744fc1e651ea0ae030
Michael Kim <m.kim4247@gmail.com>
2019-02-23 22:40:19 -0500

    changed user params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -48,7 +48,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username, :password, :email, :pot)
+    params.require(:user).permit(:username, :password, :email, :pot, :longest_streak)
   end
 
 end

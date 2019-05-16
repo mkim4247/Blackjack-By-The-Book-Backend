505bbcc3f8ad6563316d871c61ce952c29373f90
Michael Kim <m.kim4247@gmail.com>
2019-05-16 19:35:28 -0400

    changed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://blackjack-by-the-book-frontend.herokuapp.com/'
+    origins 'https://blackjack-by-the-book-frontend.herokuapp.com/', 'https://blackjack-by-the-book-frontend.herokuapp.com/login',
+    'https://blackjack-by-the-book-frontend.herokuapp.com/new'
 
     resource '*',
       headers: :any,

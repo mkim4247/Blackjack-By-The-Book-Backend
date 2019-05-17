98577b7c14214385c50b1c0b965f6a6f08b6c857
Michael Kim <m.kim4247@gmail.com>
2019-05-16 21:00:48 -0400

    trying to get env var from heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::API
 
   def secret_key
-    Rails.application.credentials.secret_key_base
+    # Rails.application.credentials.secret_key_base
+    ENV['SECRET_KEY']
   end
 
   def encode(payload)
